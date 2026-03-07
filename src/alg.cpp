1da3a5b8942f3c722658fc903e5c2418f738b19a
ardaelam <artem.elesin2007@gmail.com>
2026-03-07 20:37:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,55 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double result = 1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+    result *= value;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / (double)fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        sum += calcItem(x, n);
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+    for (uint16_t k = 0; k < count; ++k) {
+        uint16_t n = 2 * k + 1;
+        double term = calcItem(x, n);
+        if (k % 2 == 1) {
+            term = -term;
+        }
+        sum += term;
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+    for (uint16_t k = 0; k < count; ++k) {
+        uint16_t n = 2 * k;
+        double term = calcItem(x, n);
+        if (k % 2 == 1) {
+            term = -term;
+        }
+        sum += term;
+    }
+    return sum;
 }
